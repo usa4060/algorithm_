7afb547507f17ee872deeb6b2a3456932b41568f
usa4060 <cocoa9805@naver.com>
2022-01-03 16:07:18 +0900

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by admin on 2022/01/03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
