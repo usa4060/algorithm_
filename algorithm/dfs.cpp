dcba182e57c83f1ca282e90cc12eded245baa011
usa4060 <cocoa9805@naver.com>
2022-01-05 10:27:25 +0900

    DFS

--- algorithm/dfs.cpp
+++ algorithm/dfs.cpp
@@ -8,11 +8,15 @@ bool c[8];
 vector<int> a[8];
 
 void dfs(int x){
-    if(c[x])return;
+    if(c[x])return; // 해당 노드를 이미 방문했다는 의미미으로 바로 return
     c[x] = true;
-    cout<<x<<' ';
+    cout<<x<<' ';   // 해당 노드에는 아직 방문을 안했으므로, 해당 노드에 visit처리 해주고
+                    // 이제는 방문을 했으므로 출력처리
+    
     for(int i = 0; i < a[x].size(); i++){
-        int y = a[x][i];
-        dfs(y);
+        dfs(a[x][i]);
     }
+                    // 위의 for문을 통해, 해당 노드에 인접해있는 node를 탐색해주는 과정
+                    // 인접한 노드들을 모두 dfs에 집어넣어서, 이미 방문했는지를 확인해야함
+                    // 만일 방문했으면 위에서 바로 return처리를 해줘서 돌아오게 됨
 }
