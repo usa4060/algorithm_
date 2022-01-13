8e02ebfb3ec5d6919472ca39d0e17563b50fe438
usa4060 <cocoa9805@naver.com>
2022-01-13 14:07:41 +0900

    in_class_1

---
+++ rest_time/in_class_1/boj10952.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a, b;
+
+
+    while (1) {
+        cin >> a >> b;
+        if (a != 0 || b != 0)
+            cout << a + b << endl;
+        else
+            break;
+    }
+}
