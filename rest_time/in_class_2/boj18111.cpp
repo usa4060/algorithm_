367fd1995a75e54a3a0fabccd8c73952e1f01f61
usa4060 <cocoa9805@naver.com>
2022-01-22 12:41:46 +0900

    in_class_2

---
+++ rest_time/in_class_2/boj18111.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#define MAX 500
+using namespace std;
+int map[MAX][MAX] = {0, };
+int N, M, B;
+
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+   
+}
