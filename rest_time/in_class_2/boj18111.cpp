a091ea99eee8b94c7d9d9355e2accc24e1e037df
usa4060 <cocoa9805@naver.com>
2022-01-23 19:23:10 +0900

    in_class_2

--- rest_time/in_class_2/boj18111.cpp
+++ rest_time/in_class_2/boj18111.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
-#include <vector>
-#define MAX 500
 using namespace std;
-int map[MAX][MAX] = {0, };
-int N, M, B;
+int main() {
+    int n, m, b;
+    int map[500][500];
+    int leastTime = 0x7f7f7f7f;
+    int mostHeight;
+    cin >> n >> m >> b;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> map[i][j];
 
-
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-   
+    for (int h = 0; h <= 256; h++) {
+        int build = 0;
+        int remove = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int height = map[i][j] - h;
+                if (height > 0) remove += height;
+                else if (height < 0) build -= height;
+            }
+        }
+        //지울것 + 인벤토리 블록 >= 채울것
+        if (remove + b >= build) {
+            int time = remove * 2 + build;
+            if (leastTime >= time) {
+                leastTime = time;
+                mostHeight = h;
+            }
+        }
+    }
+    cout << leastTime << ' ' << mostHeight <<'\n';
 }
